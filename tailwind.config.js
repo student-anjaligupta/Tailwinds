2a675cfb37c77b0d637f5335f59119fa84aed150
student-anjaligupta <guptaanjali7420@gmail.com>
2024-04-10 06:55:03 +0530

    Add All File

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      spacing:{
+         13: '3.2rem'
+      },
+      fontSize:{
+        '10xl': ['9rem', { lineHeight: '1.2' }],
+      }
+    },
+  },
+  plugins: [],
+}
+
